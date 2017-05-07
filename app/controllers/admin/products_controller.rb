af77fc6929372fd87383c4f37ec5481b2c994d32
budihuang <hhppfd1984@gmail.com>
2017-05-07 23:53:16 +0800

    implement multi-pics

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -7,33 +7,61 @@ def index
  end
   def new
     @product = Product.new
+    @photo = @product.photos.build #for multi-pics
   end
 
 def edit
   @product = Product.find(params[:id])
 end
 def update
-  @product = Product.find(params[:id])
-  if @product.update(product_params)
-    redirect_to admin_products_path
-  else
-    render :edit
+    @product = Product.find(params[:id])
+
+    if params[:photos] != nil
+      @product.photos.destroy_all #need to destroy old pics first
+
+      params[:photos]['avatar'].each do |a|
+        @picture = @product.photos.create(:avatar => a)
+      end
+
+      @product.update(product_params)
+      redirect_to admin_products_path
+
+    elsif @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
   end
-end
   def create
     @product = Product.new(product_params)
 
     if @product.save
+
+      if params[:photos] != nil
+        params[:photos]['avatar'].each do |a|
+          @photo = @product.photos.create(:avatar => a)
+        end
+      end
       redirect_to admin_products_path
     else
       render :new
     end
   end
 
+
+  def destroy
+    @product.destroy
+    redirect_to admin_products_path
+  end
+  private
+  def find_product
+     @product = Product.find(params[:id])
+  end
+
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
-    params.require(:product).permit(:title, :description, :quantity, :price, :image)
+
+    params.require(:product).permit(:title, :description, :quantity, :price, :image, :detail)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
+  has_many :photos
   mount_uploader :image, ImageUploader
+  accepts_nested_attributes_for :photos
 end
