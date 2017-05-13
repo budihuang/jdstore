8fa2f50ecc56f1f762421fa4d3e3e8bacb8571fc
budihuang <hhppfd1984@gmail.com>
2017-05-13 21:56:39 +0800

    add OrderMailer function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,14 +8,19 @@ class OrdersController < ApplicationController
 
    if @order.save
 
-        current_cart.cart_items.each do |cart_item|
+      current_cart.cart_items.each do |cart_item|
         product_list = ProductList.new
         product_list.order = @order
         product_list.product_name = cart_item.product.title
         product_list.product_price = cart_item.product.price
         product_list.quantity = cart_item.quantity
         product_list.save
+
       end
+        current_cart.clean!
+        OrderMailer.notify_order_placed(@order).deliver!
+
+
      redirect_to order_path(@order.token)
    else
      render 'carts/checkout'
@@ -28,21 +33,21 @@ class OrdersController < ApplicationController
      @order = Order.find_by_token(params[:id])
      @product_lists = @order.product_lists
    end
-   def pay_with_alipay
-       @order = Order.find_by_token(params[:id])
-       @order.set_payment_with!("alipay")
-       @order.pay!
+ def pay_with_alipay
+     @order = Order.find_by_token(params[:id])
+     @order.set_payment_with!("alipay")
+     @order.pay!
 
-       redirect_to order_path(@order.token), notice: "使用支付宝成功完成付款"
-     end
+     redirect_to order_path(@order.token), notice: "使用支付宝成功完成付款"
+   end
 
-     def pay_with_wechat
-       @order = Order.find_by_token(params[:id])
-       @order.set_payment_with!("wechat")
-       @order.pay!
+   def pay_with_wechat
+     @order = Order.find_by_token(params[:id])
+     @order.set_payment_with!("wechat")
+     @order.pay!
 
-       redirect_to order_path(@order.token), notice: "使用微信成功完成付款"
-     end
+     redirect_to order_path(@order.token), notice: "使用微信成功完成付款"
+   end
 
   private
   def order_params
