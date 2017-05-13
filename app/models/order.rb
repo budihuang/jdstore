a9e05fe36f53c87a04b491f22bfd130aed007d5a
budihuang <hhppfd1984@gmail.com>
2017-05-13 21:34:25 +0800

    add payment_method function

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,11 +7,18 @@ class Order < ApplicationRecord
    validates :shipping_name, presence: true
    validates :shipping_address, presence: true
 
-before_create :generate_token
-def generate_token
-  self.token = SecureRandom.uuid
+  before_create :generate_token
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
 
-end
+  def set_payment_with!(method)
+     self.update_columns(payment_method: method )
+   end
+
+   def pay!
+     self.update_columns(is_paid: true )
+   end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,12 @@ root 'products#index'
     end
     resources :cart_items
 
-    resources :orders
+    resources :orders do
+      member do
+        post :pay_with_alipay
+        post :pay_with_wechat
+      end
+    end
 
 
 end
